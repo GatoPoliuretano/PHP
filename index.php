d0e6dec69abe153a93e10e5af9fdceb3059c7fe0
GatoPoliuretano <datogatkm@gmail.com>
2021-11-10 20:58:57 +0100

    003

--- index.php
+++ index.php
@@ -1,39 +1,89 @@
-<?php session_start(); ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> FORMULARI PHP - 06</title>
-</head>
-<body>
-    <h2> FORMULARI </h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        Nom: <input type="text" name="nom">
-        <br><br>
-        Password: <input type="text" name="pass">
-        <br><br>
-        Formacio:
-        <br>
-        <select name="formacio">
-            <option value="ESO"> Educació Obligatoria </option>
-            <option value="FP"> Formació Professional </option>
-            <option value="BAT"> Baxillerat </option>
-        </select>
-        <br><br>
-        Idiomes:
-        <input type="checkbox" name="idiomes"> Català 
-        <input type="checkbox" name="idiomes"> Español 
-        <input type="checkbox" name="idiomes"> Anglès 
-        <input type="checkbox" name="idiomes"> Francés 
-        <input type="checkbox" name="idiomes"> Alemà 
-        <br><br>
-        Email: <input type="text" name="mail">
-        <br><br>
-        Lloc Web: <input type="text" name="domini">
-        <br><br>
-        <input type="submit" value="enviar" name="submit">
-    </form>
-</body>
+<html lang="ca" >
+    <head>
+        <meta charset="utf-8" />
+        <title>Formularis 21 </title>
+    </head>
+    <body>
+        <h1>Formulari</h1>
+        
+        <?php 
+
+            if (isset($_GET['error'])) {
+            $error = $_GET['error'];
+            
+            if ($error == 'nom') {
+                echo '<strong style="color:red">Nom incorrecte  </strong>';
+            }
+            if ($error == 'cognom') {
+                echo '<strong style="color:red">Cognom incorrecte  </strong>';
+            }
+            if ($error == 'edat') {
+                echo '<strong style="color:red">Edat incorrectea  </strong>';
+            }
+            if ($error == 'mail') {
+                echo '<strong style="color:red">Mail incorrecte  </strong>';
+            }
+            if ($error == 'pass') {
+                echo '<strong style="color:red">Password incorrecte  </strong>';
+            }
+        
+
+        }
+        ?>
+        <!-- 
+            * action a la pàgina que anirà la informació del Formulari
+            * method POST GET
+            * filtrem de la banda del client, però també del servidor
+
+
+        -->
+        
+            <form action="processar.php" method="POST">
+                Nom:
+                
+                    <label for="nom"></label>
+                    <input type="text" name="nom"  pattern="[A-Za-z]+"/>
+                       
+                <br><br>
+                Cognom:
+                
+                    <label for="cognom" require="required" pattern="[A-Za-z]+"></label>
+                    <input type="text" name="cognom"/>
+                       
+                <br><br>
+                Formació:
+                  <select name="formacio">
+                    <option value="ESO"> Educació Obligatoria </option>
+                    <option value="FP"> Formació Professional </option>
+                    <option value="BAT"> Baxillerat </option>
+                 </select>
+                 <br><br>
+                    Idiomes:
+                    <input type="checkbox" name="idiomes"> Català 
+                    <input type="checkbox" name="idiomes"> Español 
+                    <input type="checkbox" name="idiomes"> Anglès 
+                    <input type="checkbox" name="idiomes"> Francés 
+                    <input type="checkbox" name="idiomes"> Aleman
+                <br><br>
+                Email:
+                
+                    <label for="mail"></label>
+                    <input type="email" name="mail" require="required"  />
+                       
+                <br><br>
+                Password:
+                
+                    <label for="pass"></label>
+                    <input type="password" name="pass" require="required"  />
+                       
+                <br><br>
+                
+
+                <input type="submit" value="enviar"/>
+                
+
+            </form>
+        </p>
+    </body>
 </html>
